8011ab5dccaf7138bfaa22d7b2f1baeb137b7896
Aryan Chaturvedi <aryanchaturvedi1180@gmail.com>
2026-04-24 16:33:55 +0530

    Delete purchase-service directory

--- purchase-service/src/main/java/com/stockpro/purchaseservice/exception/InvalidStatusTransitionException.java
+++
@@ -1,7 +0,0 @@
-package com.stockpro.purchaseservice.exception;
-
-public class InvalidStatusTransitionException extends RuntimeException {
-    public InvalidStatusTransitionException(String message) {
-        super(message);
-    }
-}
