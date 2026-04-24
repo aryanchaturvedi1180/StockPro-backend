c29fb0cbddffc79a1fc15b8f3252c25216bbe761
Aryan Chaturvedi <aryanchaturvedi1180@gmail.com>
2026-04-24 16:25:37 +0530

    Delete supplier-service directory

--- supplier-service/src/main/java/com/stockpro/supplierservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.stockpro.supplierservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
